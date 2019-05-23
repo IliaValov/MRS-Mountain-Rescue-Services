2cb6016565012837f81f19a9972302eb5229b18d
IliaValov <iliqvulov@abv.bg>
2019-05-23 17:31:48 +0300

    Created the desktop app and added map to it

---
+++ MRSDesktopApp/Mountain-Rescue-Services/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mountain_Rescue_Services
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void GMapControl1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
